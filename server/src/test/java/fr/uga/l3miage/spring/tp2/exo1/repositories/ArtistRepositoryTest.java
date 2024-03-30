e916a10640f507485dfd89ee70c142af409fb42a
l3miage-grasiaa <Angelica-Rivera.Grasia@etu.univ-grenoble-alpes.fr>
2024-03-30 14:05:02 +0100

    test le ArtistRepositoryTest.java

--- server/src/test/java/fr/uga/l3miage/spring/tp2/exo1/repositories/ArtistRepositoryTest.java
+++ server/src/test/java/fr/uga/l3miage/spring/tp2/exo1/repositories/ArtistRepositoryTest.java
@@ -1,8 +1,13 @@
 package fr.uga.l3miage.spring.tp2.exo1.repositories;
 
+import fr.uga.l3miage.exo1.enums.GenreMusical;
+import fr.uga.l3miage.spring.tp2.exo1.models.ArtistEntity;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @AutoConfigureTestDatabase
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK, properties = "spring.jpa.database-platform=org.hibernate.dialect.H2Dialect")
@@ -11,5 +16,35 @@ public class ArtistRepositoryTest {
     @Autowired
     private ArtistRepository artistRepository;
 
+    @Test
+    void testRequestcountByGenreMusicalEquals(){
+        // given
+        ArtistEntity artistEntity = new ArtistEntity();
+        artistEntity.setName("Hugh Jackman");
+        artistEntity.setBiography("Hugh Jackman's Biography");
+        artistEntity.setGenreMusical(GenreMusical.CLASSIC);
+
+        ArtistEntity artistEntity1 = new ArtistEntity();
+        artistEntity1.setName("Iwa K");
+        artistEntity1.setBiography("Mengenal Iwa");
+        artistEntity1.setGenreMusical(GenreMusical.RAP);
+
+        ArtistEntity artistEntity2 = new ArtistEntity();
+        artistEntity2.setName("Anne Hatheway");
+        artistEntity2.setBiography("Anne Hatheway");
+        artistEntity2.setGenreMusical(GenreMusical.CLASSIC);
+
+        artistRepository.save(artistEntity);
+        artistRepository.save(artistEntity1);
+        artistRepository.save(artistEntity2);
+
+        // when
+        int countClassicArtist = artistRepository.countByGenreMusicalEquals(GenreMusical.CLASSIC);
+        int countRANDBArtist = artistRepository.countByGenreMusicalEquals(GenreMusical.RAP);
+
+        // then
+        assertEquals(2, countClassicArtist);
+        assertEquals(1, countRANDBArtist);
+    }
 
 }
